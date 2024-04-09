eec8dc9d480de2424ba20593594b0a32adadfd2f
Toma1345 <thomas.brossier@etu.univ-orleans.fr>
2024-04-09 14:38:09 +0200

    Ajout d'équipe, handball, participant, relais et volley .java

---
+++ Handball.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Handball extends Equipe {
+
+    public Handball(String nomEquipe) {
+        super(nomEquipe);
+    }
+
+    @Override
+    public List<Athlete> getMembres() {
+        return this.lesAthletes;
+    }
+
+    @Override
+    public boolean ajouteMembres(Athlete sportif) {
+        this.lesAthletes.add(sportif);
+        return true;
+    }
+
+    @Override
+    public int participer(Epreuve compet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'participer'");
+    }
+
+    @Override
+    public String obtenirNom() {
+        return this.getNomEquipe();
+    } 
+}
